8c9e9c9538e874ace45b61538e7ca099496f69f9
Yaotian666438 <1216304886@qq.com>
2025-08-15 14:10:42 +0800

    流式传输

--- src/deepagents/model.py
+++ src/deepagents/model.py
@@ -1,15 +1,17 @@
 from langchain_openai import ChatOpenAI
 import os
 
-def get_default_model():
-    # 你自己的 SiliconFlow 兼容 OpenAI 协议的 endpoint
-    # 也可以配置到环境变量
-    base_url = "https://api.siliconflow.cn/v1"
-    # 你的 token
-    api_key = "sk-cvjjbwjtrdmaweotvfowtrzmjohczaadqwqlsmrxyxyhtcgo"
+from dotenv import load_dotenv
 
+# 先加载 .env 文件
+load_dotenv()
+
+def get_default_model():
+ 
+    base_url = os.getenv("API_BASE_URL")
+    api_key = os.getenv("API_KEY")
     return ChatOpenAI(
-        model="zai-org/GLM-4.5",
+        model="o3",
         base_url=base_url,
         api_key=api_key,
         max_tokens=4096,   # 或更高，根据模型限制
